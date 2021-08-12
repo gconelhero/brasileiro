e8f160bb73a39f6c1cf10e846117c7b8e2e4f0ff
CTECHSUL <gconelhero@gmail.com>
2021-08-12 00:12:43 -0300

    Montando json, (Extração)

--- teste01.py
+++ teste01.py
@@ -1,45 +1,59 @@
-#PDFminer
-from io import StringIO
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from pdfminer.converter import TextConverter
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-
+import os
 import tabula
-
 import pandas as pd
-
 import json
+import re
 
-
-pdf = ".\\PDFs\\Jogo17.pdf"
-
-leitor = open(pdf, 'rb')
-
-parser = PDFParser(leitor)
-document = PDFDocument(parser,)
-
-output_string = StringIO()
-rsrcmgr = PDFResourceManager()
-device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
-interpreter = PDFPageInterpreter(rsrcmgr, device)
-
-for page in PDFPage.create_pages(document):
-        interpreter.process_page(page)
-saida = output_string.getvalue()
-#print(saida)
-
-leitor = tabula.read_pdf(pdf, pages='all')
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-'''
-contador = 0
-for i in leitor:
-    df = pd.DataFrame(leitor[contador])
-    print(df)
-    contador += 1
-'''
-df = pd.DataFrame(leitor[0])
-print(df)
+
+arquivos = os.listdir('.\\PDFs\\')
+for arquivo in arquivos:
+
+    pdf = f'.\\PDFs\\{arquivo}' #".\\PDFs\\Jogo17.pdf"
+
+    leitor = open(pdf, 'rb')
+    leitor = tabula.read_pdf(pdf, pages='all')
+
+    df = pd.DataFrame(leitor[0])
+    df2 = pd.DataFrame(leitor[1])
+    df3 = pd.DataFrame(leitor[2])
+    df4 = pd.DataFrame(leitor[3])
+    print(df4)
+    
+    count = 0
+    cronologia = {}
+    primeiro = {}
+    segundo = {}
+    for i in df3[df3.columns[0]][0:-1].iloc[1:]:
+        primeiro[i] = re.search('(\d+:\d+)', df3[df3.columns[1]][count + 1]).group()
+        count += 1
+    primeiro['Acrescimo'] = re.search('(\d+ min)', df3[df3.columns[3]][0:-1].iloc[4]).group()
+
+    count = 0
+    for i in df3[df3.columns[0]][0:-1].iloc[1:]:
+        segundo[i] = re.search('(\d+:\d+)', df3[df3.columns[3]][count + 1]).group()
+        count += 1
+    segundo['Acrescimo'] = re.search('(\d+ min)', df3[df3.columns[6]][0:-1].iloc[4]).group()
+    print(df3[df3.columns[0]][0:-1].iloc[4])
+
+    #print(df3)
+    cronologia['1o Tempo'] = primeiro
+    cronologia['2o Tempo'] = segundo
+    
+
+    count = 0
+    arbitragem = {}
+    for i in df2[df2.columns[0]]:
+       arbitragem[i] = df2[df2.columns[1]][count]
+       count += 1
+
+    data_re = re.search('(\d+/\d+/\d+)', df.iloc[1][1])
+    hora_re = re.search('(\d+:\d+)', df.iloc[1][1])
+    json_model = {'Jogo': {'Campeonato': f'{df.columns[1]}', 'Jogo': f'{df.loc[0][1]}',
+                    'Data:': data_re.group(), 'Horario': hora_re.group(), 
+                    'Estadio': f'{df.iloc[1][2]}', 'Rodada': df.columns[-1], 
+                    'Arbitragem': arbitragem, 'Cronologia': cronologia}
+                    }
+
+#print(json_model)
