488633fa06460350c23aea0f4de7d54758064fcf
CTECHSUL <gconelhero@gmail.com>
2021-08-17 17:43:58 -0300

    MongoDB

---
+++ mongo_load.py
@@ -0,0 +1,38 @@
+import sys, traceback, os
+import json
+from pymongo import MongoClient
+
+arquivos = os.listdir('./json_files/')
+class DataBase():
+
+    def __init__(self, collection):
+        try:
+            self.mongo_ins = MongoClient(host='192.168.0.112',
+                                        port=27017,
+                                        maxPoolSize=200)
+            self.database_name = 'brasileiro'
+            self.collection_name = collection
+            self.connect = self.mongo_ins[self.database_name]
+            self.database = self.connect[self.collection_name]
+
+        except:
+            with open('log_mongo_client.txt',  'a') as log:
+                traceback.print_exc(file=log)
+                traceback.print_exc(file=sys.stdout)
+
+    def insert(self, arquivo):
+        try:
+            with open(arquivo, 'r') as json_file:
+                json_data = json.load(json_file)
+                insert = self.database.insert_one(json_data)
+
+            return insert
+        except:
+            with open('log_mongo_client.txt',  'a') as log:
+                traceback.print_exc(file=log)
+                traceback.print_exc(file=sys.stdout)
+
+
+arquivos = os.listdir('./json_files/')
+for arquivo in arquivos:
+    DataBase('jogos').insert(f'./json_files/{arquivo}')
--- scraper.py
+++ scraper.py
@@ -7,7 +7,8 @@ from et_main import Etl
 
 flag = True
 jogo = 1
-ano = 2018
+ano = 2019
+jogo_nulo = 0
 while flag:
     try:
         url = f'https://conteudo.cbf.com.br/sumulas/{ano}/142{jogo}se.pdf'
@@ -20,12 +21,20 @@ while flag:
         try:
             if type_pdf.MIME == 'application/pdf':
                 Etl.etMain(f'jogo_{jogo}_{ano}.pdf')
+                jogo_nulo = 0
                 
 
         except:
+            jogo_nulo += 1
             os.remove(f'./PDFs/jogo_{jogo}_{ano}.pdf')
             with open('jogos_n_existente.txt', 'a') as ne:
                 ne.write(f'{url}\n')
+            if jogo > 385:
+                ano += 1
+                jogo = 0
+            if jogo_nulo == 5:
+                flag = False
+                
 
         jogo += 1
     
