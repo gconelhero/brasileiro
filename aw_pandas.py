79904dfe0dacca0d82772af153d9ef0f5b9f26d8
CTECHSUL <gconelhero@gmail.com>
2021-08-18 12:25:04 -0300

    Pandas, instruções

--- aw_pandas.py
+++ aw_pandas.py
@@ -11,5 +11,8 @@ data = list(connect.brasileiro.jogos.find(projection={'_id':False}))
 df = pd.DataFrame(list(data))
 jogos = pd.DataFrame(list(df['Jogo']))
 
+# Construíndo um dataframe a partir do objeto arbitragem.
 arbitragem = pd.DataFrame(list(jogos['Arbitragem']))
-print(arbitragem[arbitragem.columns[0:2]].iloc[0:2])
+# Printando as 2 primeiras colunas e as 10 primeiras linhas do Data Frame.
+print(arbitragem[arbitragem.columns[0:2]].iloc[0:10])
+
