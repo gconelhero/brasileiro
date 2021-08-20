8b4b8a88a0b07fe89e3b595865ab6cbdd9f7a3a0
CTECHSUL <gconelhero@gmail.com>
2021-08-20 00:01:34 -0300

    Testes

--- aw_pandas.py
+++ aw_pandas.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient
 import pandas as pd
 from pyspark import SparkContext, SparkConf
-
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
+pd.set_option('expand_frame_repr', False)
 
 connect = MongoClient(host='192.168.0.112')
 
@@ -12,7 +12,29 @@ data = list(connect.brasileiro.jogos.find(projection={'_id':False}))
 df = pd.DataFrame(list(data))
 jogos = pd.DataFrame(list(df['Jogo']))
 
-# Construíndo um dataframe a partir do objeto arbitragem.
 arbitragem = pd.DataFrame(list(jogos['Arbitragem']))
-# Printando as 2 primeiras colunas e as 10 primeiras linhas do Data Frame.
-print(arbitragem[arbitragem.columns[0:2]].iloc[0:10])
+
+arbitragem = pd.concat([jogos['No'], 
+            pd.to_datetime(jogos['Data'], format='%d/%m/%Y'), 
+            arbitragem], axis=1)
+
+#print(arbitragem[arbitragem.columns[0:3]][0:10])
+janeiro = arbitragem.loc[(arbitragem['Data'].dt.year==2020) & (arbitragem['Data'].dt.day==13)]
+janeiro = janeiro[janeiro.columns[0:7]].iloc[0:10]
+index_off = janeiro.set_index(['No', 'Data'])
+
+mandante = pd.DataFrame(list(jogos['Mandante']))
+mandante = pd.DataFrame(list(mandante['Jogadores']))
+
+# Oriented for Column
+lista_jogadores_mandantes = [i for i in mandante.iloc[0].dropna()]
+print(pd.DataFrame.from_records(lista_jogadores_mandantes))    
+
+lista_jogadores_mandantes = []
+
+for i in mandante.index:    
+    lista_jogadores_mandantes = [i for i in mandante.iloc[i].dropna()]
+    
+# From Records obrigatóriamente orientado à coluna
+# From dict orientado às
+print(pd.DataFrame.from_records(lista_jogadores_mandantes))
